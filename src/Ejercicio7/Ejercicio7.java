2211eeed76cc715b8c5774c6c4f164d7c4fe0718
Your Name <you@example.com>
2026-05-07 10:42:24 -0300

    Actualizo

--- src/Ejercicio7/Ejercicio7.java
+++ src/Ejercicio7/Ejercicio7.java
@@ -11,7 +11,6 @@ public class Ejercicio7 {
 		int cantProductos;
 		 String producto;
 		 int stockProductos;
-		int stockBajo;
 		
 		System.out.println("Ingrese la cantidad de productos: ");
 		
@@ -21,32 +20,31 @@ public class Ejercicio7 {
 
 			System.out.print("Producto " + i + ": ");
 
-			producto = entrada.next();
+			producto = entrada.nextLine();
+			
+			while (producto.isBlank()) {
+	            System.err.println("Error: El nombre del producto no puede quedar vacío. Inténtelo de nuevo:");
+	            producto = entrada.nextLine();
+	        }
 			
 			System.out.println("Ingrese la cantidad de unidades de este producto: ");
 	
 			stockProductos = entrada.nextInt();
 			
+			while (stockProductos < 0) {
+	            System.err.println("Error: cantidad de productos no válida. Ingrese un valor mayor a 0:");
+	            stockProductos = entrada.nextInt();
+	        }
+			
 			if (stockProductos <= 5) {
 				
-				stockBajo = stockProductos;
 				System.out.println("Alerta! Este producto esta en peligro de extincion.");
 				
 			}
 			
+			System.out.println("Ultimo producto: "+producto);
+			System.out.println("Stock: "+stockProductos);
 		}
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
-		
 		
 	}
 	
--- src/Ejercicio8/Ejercicio8.java
+++ src/Ejercicio8/Ejercicio8.java
@@ -6,24 +6,100 @@ public class Ejercicio8 {
 
 	public static void main(String[] args) {
 
+
+
 		Scanner entrada = new Scanner(System.in);
-		
-		String nombre;
-		int edad;
-		
-		System.out.println("Ingrese el nombre del alumno: ");
-		nombre = entrada.next();
-		
-		System.out.println("Ingrese la edad: ");
-		edad = entrada.nextInt();
-		
+
+
+
+		String nombAlumno = " ";
+
+		int edadAlumno = 0;
+
+		int cantInscriptos = 0;
+
+
+
+		System.out.println("Cuantos alumnos se van a inscribir: ");
+
+		cantInscriptos = entrada.nextInt();
+
+	
+
+
+
+		for (int i = 0; i < cantInscriptos; i++) {
+
+			entrada.nextLine();
+
+			System.out.println("Ingrese su nombre completo: ");
+
+			nombAlumno = entrada.nextLine();
+
+
+
+			while (nombAlumno.isBlank()) {
+
+				System.err.println("Error: El nombre no puede quedar vacío. Inténtelo de nuevo!:");
+
+				nombAlumno = entrada.nextLine();
+
+ 
+
+			}
+
+			System.out.println("Ingrese su edad: ");
+
+			edadAlumno = entrada.nextInt();
+
+
+
+			if (edadAlumno < 0 || edadAlumno > 100) {
+
+				System.err.println("Error: Edad no válida. Ingrese una edad entre 0-100	:");
+
+				edadAlumno = entrada.nextInt();
+
+			}
+
+
 
 		
-		
-		
-		
-		
-		
+
+		}
+
+
+
+		System.out.println("---LISTA FINAL---");
+
+
+
+		System.out.println("-Cantidad " + cantInscriptos + " de inscripciones");
+
+		System.out.println("-Alumno/a: " + nombAlumno);
+
+		System.out.println("-Edad: " + edadAlumno + " Años ");
+
+
+
+		if (edadAlumno < 18) {
+
+			System.out.println("-El alumno/a es menor de edad. ");
+
+		}
+
+
+
+		if (edadAlumno >= 18) {
+
+			System.out.println("-El alumno/a es mayor de edad. ");
+
+		}
+
+
+
 	}
+
+
 
 }
